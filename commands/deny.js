f5669dcf1673c58c22321192d8f32444f194a399
Facto <33882218+Sniped@users.noreply.github.com>
2019-02-20 23:41:16 -0500

    commenting is now removed everywhere

--- commands/deny.js
+++ commands/deny.js
@@ -1,60 +1,31 @@
 const Discord = require('discord.js');
 module.exports = {
     run: async (client, msg, args) => {
-        if (!args[0]) return msg.channel.send(':x: Invalid arguments! Valid arguments are: `suggestion`, and `comment`.');
-        if (args[0] == 'suggestion') {
-            if (!args[1]) return msg.channel.send(':x: You must supply an ID!');
-            if (Number(args[1]) == NaN) return msg.channel.send(':x: The ID must be a number!');
-            const id = parseInt(args[1], 10);
-            const data = await client.db.table('suggestions').get(id).run();
-            if (!data) return msg.channel.send(':x: Invalid ID!');
-            const reason = args.slice(2).join(' ');
-            if (!reason) return msg.channel.send(':x: You must supply a reason.');
-            if (data.status == 'PENDING') {
-                const embed = new Discord.RichEmbed()
-                .setTitle('Your suggestion was denied by a council member.')
-                .setDescription(`**ID**: ${data.sid}\n\n**Suggestion:** ${data.suggestion}\n\n**Reason**: ${reason}`)
+        if (!args[0]) return msg.channel.send(':x: You must supply an ID!');
+        if (Number(args[0]) == NaN) return msg.channel.send(':x: The ID must be a number!');
+        const id = parseInt(args[0], 10);
+        const data = await client.db.table('suggestions').get(id).run();
+        if (!data) return msg.channel.send(':x: Invalid ID!');
+        const reason = args.slice(1).join(' ');
+        if (!reason) return msg.channel.send(':x: You must supply a reason.');
+        if (data.status == 'PENDING') {
+            const embed = new Discord.RichEmbed()
+            .setTitle('Your suggestion was denied by a council member.')
+            .setDescription(`**ID**: ${data.sid}\n\n**Suggestion:** ${data.suggestion}\n\n**Reason**: ${reason}`)
+            .setColor('#c56868');
+            const author = client.users.get(data.author.id);
+            author.send(embed);
+            client.db.table('suggestions').get(id).update({ status: 'DENIED', reason: reason, member: { id: msg.author.id, name: msg.author.username } }).run();
+            const notificationchannel = client.channels.find(channel => channel.id === client.config.notifysuggestionschannel);
+            notificationchannel.fetchMessage(data.notifymsg.id).then(nmsg => {
+                const notificationembed = new Discord.RichEmbed()
+                .setTitle(`A suggestion made by ${author.username} was denied by ${msg.author.username}`)
+                .setDescription(`**ID**: ${data.sid}\n\n**Suggestion**: ${data.suggestion}\n\n**Reason**: ${reason}`)
                 .setColor('#c56868');
-                const author = client.users.get(data.author.id);
-                author.send(embed);
-                client.db.table('suggestions').get(id).update({ status: 'DENIED', reason: reason, member: { id: msg.author.id, name: msg.author.username } }).run();
-                const notificationchannel = client.channels.find(channel => channel.id === client.config.notifysuggestionschannel);
-                notificationchannel.fetchMessage(data.notifymsg.id).then(nmsg => {
-                    const notificationembed = new Discord.RichEmbed()
-                    .setTitle(`A suggestion made by ${author.username} was denied by ${msg.author.username}`)
-                    .setDescription(`**ID**: ${data.sid}\n\n**Suggestion**: ${data.suggestion}\n\n**Reason**: ${reason}`)
-                    .setColor('#c56868');
-                    nmsg.edit(notificationembed);
-                });
-                msg.channel.send(`:white_check_mark: Successfully denied suggestion ${args[1]}`);
-            } else return msg.channel.send(':x: The suggestion has already been dealt with by another member of the commitee!');
-        } else if (args[0] == 'comment') {
-            if (!args[1]) return msg.channel.send(':x: You must supply an ID!');
-            if (Number(args[1]) == NaN) return msg.channel.send(':x: The ID must be a number!');
-            const id = parseInt(args[1], 10);
-            const data = await client.db.table('comments').get(id).run();
-            if (!data) return msg.channel.send(':x: Invalid ID!');
-            const reason = args.slice(2).join(' ');
-            if (!reason) return msg.channel.send(':x: You must supply a reason.');
-            if (data.status == 'PENDING') {
-                const embed = new Discord.RichEmbed()
-                .setTitle('Your comment was denied by a council member.')
-                .setDescription(`**ID**: ${data.cid}\n\n**Comment**:${data.comment}\n\n**Reason**: ${reason}`)
-                .setColor('#c56868');
-                const author = client.users.get(data.author.id);
-                author.send(embed);
-                client.db.table('comments').get(id).update({ status: 'DENIED', reason: reason, member: { id: msg.author.id, name: msg.author.username } }).run();
-                const notificationschannel = client.channels.find(channel => channel.id === client.config.notifycommentschannel);
-                notificationschannel.fetchMessage(data.notifymsg.id).then(nmsg => {
-                    const notificationembed = new Discord.RichEmbed()
-                    .setTitle(`A comment made by ${author.username} was denied by ${msg.author.username}`)
-                    .setDescription(`**ID**: ${data.cid}\n\n**Comment**: ${data.comment}\n\n**Reason**: ${reason}`)
-                    .setColor('#c56868');
-                    nmsg.edit(notificationembed);
-                });
-                msg.channel.send(`:white_check_mark: Successfully denied comment ${args[1]}`);    
-            } else return msg.channel.send(':x: The comment has already been dealt with by another member of the commitee!');
-        }
+                nmsg.edit(notificationembed);
+            });
+            msg.channel.send(`:white_check_mark: Successfully denied suggestion ${args[1]}`);
+        } else return msg.channel.send(':x: The suggestion has already been dealt with by another member of the commitee!');
     },
     meta: {
         aliases: ['deny'],
--- commands/lookup.js
+++ commands/lookup.js
@@ -1,66 +1,34 @@
 const Discord = require('discord.js');
 module.exports = {
     run: async (client, msg, args) => {
-        if (!args[0]) return msg.channel.send(':x: Invalid argument! Valid arguments are `suggestion` and `comment`.');
-        if (args[0] == 'suggestion') {
-            if (!args[1]) return msg.channel.send(':x: You must supply an ID.');
-            if (Number(args[1]) == NaN) return msg.channel.send(':x: The ID must be a number.');
-            const id = parseInt(args[1], 10);
-            const data = await client.db.table('suggestions').get(id).run();
-            if (!data) return msg.channel.send(':x: Invalid ID!');
-            const author = client.users.find(user => user.id === data.author.id);
-            if (data.status == 'PENDING') {
-                const pembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.sid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#83c5ca');
-                msg.channel.send(pembed);
-            } else if (data.status == 'ACCEPTED') {
-                const aembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.sid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Accepter**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#9ad68f');
-                msg.channel.send(aembed);
-            } else if (data.status == 'DENIED') {
-                const dembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.sid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Denier**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#c56868');
-                msg.channel.send(dembed);
-            }
-        } else if (args[0] == 'comment') {
-            if (!args[1]) return msg.channel.send(':x: You must supply an ID.');
-            if (Number(args[1]) == NaN) return msg.channel.send(':x: The ID must be a number.');
-            const id = parseInt(args[1], 10);
-            const data = await client.db.table('comments').get(id).run();
-            const author = client.users.find(user => user.id === data.author.id);
-            if (!data) return msg.channel.send(':x: Invalid ID!');
-            if (data.status == 'PENDING') {
-                const pembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.cid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Status**: ${data.status}\n\n**Comment**: ${data.comment}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#83c5ca');
-                msg.channel.send(pembed);
-            } else if (data.status == 'ACCEPTED') {
-                const aembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.cid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Accepter**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Comment**: ${data.comment}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#9ad68f');
-                msg.channel.send(aembed);
-            } else if (data.status == 'DENIED') {
-                const dembed = new Discord.RichEmbed()
-                .setTitle(`Showing data for suggestion ${data.cid}`)
-                .setDescription(`**Author**: ${data.author.name}\n\n**Denier**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Comment**: ${data.comment}`)
-                .setThumbnail(author.avatarURL)
-                .setColor('#c56868');
-                msg.channel.send(dembed);
-            }
-        } else return msg.channel.send(':x: Invalid argument! Valid arguments are `suggestion` and `comment`.');
+        if (!args[1]) return msg.channel.send(':x: You must supply an ID.');
+        if (Number(args[1]) == NaN) return msg.channel.send(':x: The ID must be a number.');
+        const id = parseInt(args[1], 10);
+        const data = await client.db.table('suggestions').get(id).run();
+        if (!data) return msg.channel.send(':x: Invalid ID!');
+        const author = client.users.find(user => user.id === data.author.id);
+        if (data.status == 'PENDING') {
+            const pembed = new Discord.RichEmbed()
+            .setTitle(`Showing data for suggestion ${data.sid}`)
+            .setDescription(`**Author**: ${data.author.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
+            .setThumbnail(author.avatarURL)
+            .setColor('#83c5ca');
+            msg.channel.send(pembed);
+        } else if (data.status == 'ACCEPTED') {
+            const aembed = new Discord.RichEmbed()
+            .setTitle(`Showing data for suggestion ${data.sid}`)
+            .setDescription(`**Author**: ${data.author.name}\n\n**Accepter**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
+            .setThumbnail(author.avatarURL)
+            .setColor('#9ad68f');
+            msg.channel.send(aembed);
+        } else if (data.status == 'DENIED') {
+            const dembed = new Discord.RichEmbed()
+            .setTitle(`Showing data for suggestion ${data.sid}`)
+            .setDescription(`**Author**: ${data.author.name}\n\n**Denier**: ${data.member.name}\n\n**Status**: ${data.status}\n\n**Suggestion**: ${data.suggestion}`)
+            .setThumbnail(author.avatarURL)
+            .setColor('#c56868');
+            msg.channel.send(dembed);
+        }
     },
     meta: {
         aliases: ['lookup'],
